67ef6937832827bc2e8f6e3834a5cf8d3f8056d3
Benjamin Mårtensson <benjamin.martensson@nrk.no>
2019-07-30 23:02:26 +0200

    improving sync to create

--- lastpass/create.go
+++ lastpass/create.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -25,28 +26,46 @@ func (c *Client) Create(s Secret) (Secret, error) {
 		var err = errors.New(errbuf.String())
 		return s, err
 	}
-	time.Sleep(time.Second * 5) // Need to finish sync with upstream/lastpass before we get actual ID.
-	cmd = exec.Command("lpass", "show", s.Name, "--json", "-x")
 	var outbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-	err = cmd.Run()
-	if err != nil {
-		var err = errors.New(errbuf.String())
-		return s, err
-	}
 	var secrets []Secret
-	err = json.Unmarshal(outbuf.Bytes(), &secrets)
-	if err != nil {
-		return s, err
+	// because of the ridiculous way lpass sync works we will need to retry until we get our ID.
+	// see open issue at https://github.com/lastpass/lastpass-cli/issues/450
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second * 2)
+		errbuf.Reset()
+		outbuf.Reset()
+		cmd = exec.Command("lpass", "sync")
+		cmd.Stderr = &errbuf
+		err = cmd.Run()
+		if err != nil {
+			var err = errors.New(errbuf.String())
+			return s, err
+		}
+		cmd = exec.Command("lpass", "show", "--sync=now", s.Name, "--json", "-x")
+		cmd.Stdout = &outbuf
+		cmd.Stderr = &errbuf
+		err = cmd.Run()
+		if err != nil {
+			if !strings.Contains(errbuf.String(), "Could not find specified account") {
+				var err = errors.New(errbuf.String())
+				return s, err
+			}
+			continue
+		}
+		err = json.Unmarshal(outbuf.Bytes(), &secrets)
+		if err != nil {
+			return s, err
+		}
+		if len(secrets) > 1 {
+			err := errors.New("more than one secret with same name, unable to determine ID")
+			return s, err
+		}
+		if secrets[0].ID == "0" {
+			// sync is still not done with upstream.
+			continue
+		}
+		return secrets[0], nil
 	}
-	if len(secrets) > 1 {
-		err := errors.New("more than one secret with same name, unable to determine ID")
-		return s, err
-	}
-	if secrets[0].ID == "0" {
-		err := errors.New("got invalid ID 0, problem with lastpass sync")
-		return secrets[0], err
-	}
-	return secrets[0], nil
+	err = errors.New("timeout, unable to create new secret")
+	return s, err
 }
