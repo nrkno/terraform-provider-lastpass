4cdbd6aee3f2c5a66936de427677703c206ac86e
Benjamin Mårtensson <benjamin.martensson@nrk.no>
2019-07-30 23:02:58 +0200

    adding more tests

--- resource_secret_test.go
+++ resource_secret_test.go
@@ -12,7 +12,6 @@ import (
 
 func TestAccResourceSecret_Basic(t *testing.T) {
 	var secret lastpass.Secret
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -30,6 +29,26 @@ func TestAccResourceSecret_Basic(t *testing.T) {
 						"lastpass_secret.foobar", "password", "hunter2"),
 					resource.TestCheckResourceAttr(
 						"lastpass_secret.foobar", "note", "secret note"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecret_Generated(t *testing.T) {
+	var secret lastpass.Secret
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccResourceSecretDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceSecretConfig_generated,
+				Check: resource.ComposeTestCheckFunc(
+					testAccResourceSecretExists("lastpass_secret.foobar", &secret),
+					testAccResourceSecretGeneratedLength("lastpass_secret.foobar", 24),
+					resource.TestCheckResourceAttr(
+						"lastpass_secret.foobar", "name", "terraform-provider-lastpass resource generated test"),
 				),
 			},
 		},
@@ -73,10 +92,37 @@ func testAccResourceSecretExists(n string, secret *lastpass.Secret) resource.Tes
 	}
 }
 
+func testAccResourceSecretGeneratedLength(n string, length int) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		provider := testAccProvider.Meta().(*lastpass.Client)
+		secrets, err := provider.Read(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+		if len(secrets) != 1 && len(secrets[0].Password) != length {
+			return fmt.Errorf("password has wrong length %d", len(secrets[0].Password))
+		}
+		return nil
+	}
+}
+
 const testAccResourceSecretConfig_basic = `
 resource "lastpass_secret" "foobar" {
     name = "terraform-provider-lastpass resource basic test"
     username = "gopher"
     password = "hunter2"
     note = "secret note"
+}`
+
+const testAccResourceSecretConfig_generated = `
+resource "lastpass_secret" "foobar" {
+	name = "terraform-provider-lastpass resource generated test"
+	generate {
+        length = 24
+        use_symbols = false
+    }
 }`
