059e849d2ebe2f7bd6e351c79f49d511fb66c269
Benjamin Mårtensson <benjamin.martensson@nrk.no>
2019-11-08 15:03:35 +0100

    changing name will force new (destroy and create)

--- resource_secret.go
+++ resource_secret.go
@@ -24,6 +24,7 @@ func ResourceSecret() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"fullname": {
 				Type:     schema.TypeString,
